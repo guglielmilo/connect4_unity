00a15a4d7708027f470a531de62320c0a2a72377
Guillaume <guillaume.militello@gmail.com>
2021-12-24 07:26:58 -0500

    Add basic win screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject coinPlayer1;
     public GameObject coinPlayer2;
+    
+    public GameObject winCanvas;
+    private int player1score = 0;
+    private int player2score = 0;
 
     private State state = new State('1');
 
@@ -14,6 +19,7 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Start");
         Debug.Log("state:" + state);
+        winCanvas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -43,6 +49,42 @@ public class GameManager : MonoBehaviour
         if (state.Done)
         {
             Debug.Log("board done, winner:" + Player.playerToString(state.PlayerWin));
+            ShowWinScreen();
         }
     }
+
+    private void ShowWinScreen()
+    {
+        Transform playerWonTextTransform = winCanvas.transform.Find("PlayerWonText");
+        Text playerWonText = playerWonTextTransform.GetComponent<Text>();
+
+        if (state.PlayerWin == '1')
+        {
+            player1score++;
+            playerWonText.text = "Player 1 Won";
+            playerWonText.color = Color.red;
+        }
+        else if (state.PlayerWin == '2')
+        {
+            player2score++;
+            playerWonText.text = "Player 2 Won";
+            playerWonText.color = Color.yellow;
+        }
+        else
+        {
+            playerWonText.text = "Draw";
+            playerWonText.color = Color.black;
+        }
+
+        Transform player1ScoreTextTransform = winCanvas.transform.Find("Player1ScoreText");
+        Text player1ScoreText = player1ScoreTextTransform.GetComponent<Text>();
+        player1ScoreText.text = player1score.ToString();
+        
+        Transform player2ScoreTextTransform = winCanvas.transform.Find("Player2ScoreText");
+        Text player2ScoreText = player2ScoreTextTransform.GetComponent<Text>();
+        player2ScoreText.text = player2score.ToString();
+
+        winCanvas.SetActive(true);
+    }
+
 }
